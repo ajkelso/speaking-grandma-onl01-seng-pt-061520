d36871ac5223c26bcf7fcc1197f46fd4467ca964
A. Kelso <ajkelso@gmail.com>
2020-06-16 17:20:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    return ""
+    return "I LOVE YOU TOO PUMPKIN!"
     elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   else
